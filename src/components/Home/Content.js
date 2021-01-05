1dff801a5a064189e5a799b437e486fa62b728fb
jsusilo <enr@bk.ru>
2021-01-05 16:39:21 +0500

    Home content

---
+++ src/components/Home/Content.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Products from "./Products";
+import NumberOneSupplier from "./NumberOneSupplier";
+import Quote from "./Quote";
+import Contact from "./Contact";
+
+class Content extends React.Component {
+  render() {
+    return (
+      <main>
+        {}
+        <Products></Products>
+        {}
+        {}
+        <NumberOneSupplier></NumberOneSupplier>
+        {}
+        {}
+        <Quote></Quote>
+        {}
+        {}
+        <Contact></Contact>
+        {}
+      </main>
+    );
+  }
+}
+
+export default Content;
