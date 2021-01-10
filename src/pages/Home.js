dbf862642976fc209a30cca9a02dea92e9dc8649
jsusilo <enr@bk.ru>
2021-01-10 12:32:32 +0500

    main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,14 @@
 import React from "react";
 
-import Slider from "../components/Home/Slider";
-import Header from "../components/Header";
-import About from "../components/Home/About";
-import Over20Years from "../components/Home/Over20Years";
-import Owl from "../components/Home/Owl";
-import Service from "../components/Home/Service";
-import News from "../components/Home/News";
-import Contact from "../components/Home/Contact";
-import Footer from "../components/Footer";
+import Header from "../components/Home/Header";
+import Content from "../components/Home/Content";
+import Footer from "../components/Home/Footer";
 
 const Home = () => {
   return (
-    <div className="page animated">
+    <div className="page">
       <Header />
-      <Slider />
-      <About />
-      <Over20Years />
-      <Owl />
-      <Service />
-      <News />
-      <Contact />
+      <Content />
       <Footer />
     </div>
   );
